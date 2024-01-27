eb0ee50257ca9bcd4bde8f59bd643b4a3590bc20
lapeko <lapeko88@gmail.com>
2024-01-27 07:57:09 +0100

    create banner component using material theming

---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from "@angular/core";
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss'],
+})
+export class BannerComponent {
+  @Input()
+  heading: string = "";
+
+  @Input()
+  message: string = "";
+}
