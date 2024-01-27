c7cb7e158b21570cd78557bcbaae1524eefb9b91
lapeko <lapeko88@gmail.com>
2024-01-27 07:57:09 +0100

    create banner component using material theming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { SharedMaterialModule } from './shared-material/shared-material.module';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BannerComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
