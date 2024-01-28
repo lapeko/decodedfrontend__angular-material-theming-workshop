bc44c883eddcfb4f6613260faee3990587a8131e
lapeko <lapeko88@gmail.com>
2024-01-28 07:36:43 +0100

    add dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
+import { MatSelectChange } from "@angular/material/select";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
+  ) {
+  }
+
+  ngOnInit() {
+    this.renderer.addClass(this.document.body, "app-dark-theme");
+  }
+
+  onThemeChange({source}: MatSelectChange) {
+    if (source.value === "light") {
+      this.renderer.removeClass(this.document.body, "app-dark-theme");
+      this.renderer.addClass(this.document.body, "app-light-theme");
+    } else {
+      this.renderer.removeClass(this.document.body, "app-light-theme");
+      this.renderer.addClass(this.document.body, "app-dark-theme");
+    }
+  }
 }
